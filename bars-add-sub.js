c815d85b7e063ef458acbb43f5f10f4e9a122dfb
Mason Chinkin <32528966+gumby7@users.noreply.github.com>
2017-11-19 16:15:18 -0800

    WIP with add/sub/sort buttons

--- bars-add-sub.js
+++ bars-add-sub.js
@@ -1,18 +1,49 @@
-  var w = 600 //width variabelized
-  var h = 300 //height variabelized
-  var margin = 60
+  //Defined functions
+  var w = 600; //svg width
+  var h = 300; //svg height
+  var margin = 60; //svg margin
 
-  var maxValue = 40
+  //properties of mouseout
+  var barMouseOut = function (d) {
+      d3.select(this)
+          .transition('orangeHover')
+          .duration(250)
+          .attr('fill', "rgb(0,0, " + Math.floor(y(d.value)) + ")");
 
-  /*var barDataset = {}; //Initialize empty array
-  for (var i = 0; i < 30; i++) { //Loop X times
-      var newNumber = Math.round(Math.random() * maxValue); //Math.random creates almost 0 to almost 1
-      var lastKeyValue = i + 1;
-      barDataset.push({
-          key: lastKeyValue,
-          value: newNumber
-      });
-  };*/
+      //Hide the tooltip
+      d3.select("#tooltip").classed("hidden", true);
+  };
+
+  //properties of mouseover
+  var barMouseOver = function (d) {
+      d3.select(this)
+          .attr('fill', 'orange');
+
+      var xpos = parseFloat(d3.select(this).attr('x')) + x.bandwidth() / 2;
+      var ypos = parseFloat(d3.select(this).attr('y')) / 2 + h / 2;
+
+      //Update the tooltip position and value
+      d3.select('#tooltip')
+          .style("left", xpos + "px")
+          .style("top", ypos + "px")
+          .select("#value")
+          .text(d.value);
+
+      //Show the tooltip
+      d3.select('#tooltip').classed("hidden", false);
+  };
+
+  //is it sorted?
+  var sorted = false;
+
+  /*var exitLeft = .exit() //EXIT
+      .transition()
+      .duration(750)
+      .ease(d3.easeElasticOut)
+      .attr('x', -x.bandwidth()) //EXIT STAGE LEFT
+      .remove();*/
+
+  var maxValue = 40; //max value for any randomiz data
 
   var barDataset = [{ key: 0, value: 5 }, //dataset is now an array of objects.
       { key: 1, value: 10 }, //Each object has a 'key' and a 'value'.
@@ -38,7 +69,7 @@
 
   var key = function (d) {
       return d.key;
-  }
+  };
 
   //SCALES
 
@@ -58,10 +89,10 @@
   var svg = d3.select("body")
       .append("svg")
       .attr("width", w)
-      .attr("height", h);
+      .attr("height", h); //generate SVG element
 
   var bars = svg.selectAll("rect")
-      .data(barDataset, key)
+      .data(barDataset, key);
 
   bars.enter()
       .append("rect")
@@ -76,42 +107,15 @@
           return y(d.value);
       })
       .attr('fill', function (d) {
-          return "rgb(0,0, " + Math.round(y(d.value)) + ")";
+          return "rgb(0,0, " + Math.floor(y(d.value)) + ")";
       })
-      .on('mouseover', function () {
-          d3.select(this)
-              .attr('fill', 'orange');
-
-          var xpos = parseFloat(d3.select(this).attr('x')) + x.bandwidth() / 2;
-          var ypos = parseFloat(d3.select(this).attr('y')) / 2 + h / 2;
-
-          //Update the tooltip position and value
-          d3.select('#tooltip')
-              .style("left", xpos + "px")
-              .style("top", ypos + "px")
-              .select("#value")
-              .text(function (d) {
-                  return d.value
-              });
-
-          //Show the tooltip
-          d3.select('#tooltip').classed("hidden", false);
-
-      })
-      .on('mouseout', function (d) {
-          d3.select(this)
-              .transition('orangeHover')
-              .duration(250)
-              .attr('fill', "rgb(0,0, " + Math.round(y(d.value)) + ")");
-      })
-      .on('click', function () {
-          sortBars()
-      });
+      .on('mouseover', barMouseOver)
+      .on('mouseout', barMouseOut);
 
   //LABELS
 
   var text = svg.selectAll("text")
-      .data(barDataset, key)
+      .data(barDataset, key);
 
   text.enter()
       .append("text")
@@ -124,178 +128,197 @@
       .attr("y", function (d) {
           if (d.value >= 6) {
               return h - y(d.value) + 14;
-          } else { return h - y(d.value) - 4 }
+          } else { return h - y(d.value) - 4; }
       })
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "11px")
+      .attr("class", "barLabel")
       .attr("fill", function (d) {
           if (d.value >= 6) {
-              return "white"
-          } else { return "black" }
-      })
-      .attr('text-anchor', 'middle')
-      .style('pointer-events', 'none');
-
-  //TRANSITION
-
-  d3.selectAll('p')
-      .on('click', function () {
-
-          var paragraphID = d3.select(this).attr('id');
-
-          //ADD OR REMOVE ONE DATUM TO barDataset
-          if (paragraphID == "add") {
-              var newNumber = Math.round(Math.random() * maxValue); //Math.random creates almost 0 to almost 1
-              var lastKeyValue = barDataset[barDataset.length - 1].key
-              barDataset.push({
-                  key: lastKeyValue + 1,
-                  value: newNumber //Add new number to array
-              })
-          }
-          if (paragraphID == "remove") {
-              barDataset.shift()
-          }
-
-
-          /*HAVING SORT BUTTON ISSUES...
-          if (paragraphID == "sort") {
-              sortBars()
-          }*/
-
-          //UPDATE SCALES
-          x.domain(d3.range(barDataset.length));
-          y.domain([0, d3.max(barDataset, function (d) {
-              return d.value
-          })]);
-
-          //Transition BARS
-
-          var bars = svg.selectAll("rect") //SELECT
-              .data(barDataset, key);
-
-          bars.enter() //ENTER
-              .append("rect")
-              .attr("x", w)
-              .attr("y", function (d) {
-                  return h - y(d.value);
-              })
-              .attr("width", x.bandwidth())
-              .attr("height", function (d) {
-                  return y(d.value);
-              })
-              .attr('fill', function (d) {
-                  return "rgb(0,0, " + Math.round(y(d.value)) + ")";
-              })
-              .merge(bars)
-              .transition()
-              .duration(750)
-              .ease(d3.easeElasticOut)
-              .attr('x', function (d, i) {
-                  return x(i)
-              })
-              .attr("y", function (d) {
-                  return h - y(d.value);
-              })
-              .attr("width", x.bandwidth())
-              .attr("height", function (d) {
-                  return y(d.value)
-              })
-              .attr('fill', function (d) {
-                  return "rgb(0,0, " + Math.round(y(d.value)) + ")";
-              });
-
-          bars.exit() //EXIT
-              .transition()
-              .duration(750)
-              .ease(d3.easeElasticOut)
-              .attr('x', -x.bandwidth()) //EXIT STAGE LEFT
-              .remove();
-
-          //TRANSITION LABELS
-
-          var text = svg.selectAll("text")
-              .data(barDataset, key)
-
-          text.enter()
-              .append("text")
-              .text(function (d) {
-                  return d.value;
-              })
-              .attr("x", w + (x.bandwidth() / 2))
-              .attr("y", function (d) {
-                  if (d.value >= 6) {
-                      return h - y(d.value) + 14;
-                  } else {
-                      return h - y(d.value) - 4
-                  }
-              })
-              .attr("font-family", "sans-serif")
-              .attr("font-size", "11px")
-              .attr("fill", function (d) {
-                  if (d.value >= 6) {
-                      return "white"
-                  } else {
-                      return "black"
-                  }
-              })
-              .merge(text)
-              .transition()
-              .duration(750)
-              .ease(d3.easeElasticOut)
-              .text(function (d) {
-                  return d.value;
-              })
-              .attr("x", function (d, i) {
-                  return x(i) + x.bandwidth() / 2;
-              })
-              .attr("y", function (d) {
-                  if (d.value >= 6) {
-                      return h - y(d.value) + 14;
-                  } else {
-                      return h - y(d.value) - 4
-                  }
-              })
-              .attr('text-anchor', 'middle');
-
-          text.exit() //EXIT
-              .transition()
-              .duration(750)
-              .ease(d3.easeElasticOut)
-              .attr('x', -x.bandwidth()) //EXIT STAGE LEFT
-              .remove();
+              return "white";
+          } else { return "black"; }
       });
 
-  //Defined functions
-  var sortOrder = false;
-  var sortBars = function () {
+  //BUTTONS
 
-      sortOrder = !sortOrder;
+  function addBar() {
+      var newNumber = Math.floor(Math.random() * maxValue);
+      var lastKeyNumber = d3.max(barDataset, function (d) {
+          return d.key
+      });
+      barDataset.push({
+          key: lastKeyNumber + 1,
+          value: newNumber, //Add new number to array
+      });
+
+      //UPDATE SCALES
+      x.domain(d3.range(barDataset.length));
+      y.domain([0, d3.max(barDataset, function (d) {
+          return d.value;
+      })]);
+
+      var bars = svg.selectAll("rect") //SELECT
+          .data(barDataset);
+
+      //Transition BARS
+
+      bars.enter() //ENTER
+          .append("rect")
+          .attr("x", w)
+          .attr("y", function (d) {
+              return h - y(d.value);
+          })
+          .attr("width", x.bandwidth())
+          .attr("height", function (d) {
+              return y(d.value);
+          })
+          .attr('fill', function (d) {
+              return "rgb(0,0, " + Math.floor(y(d.value)) + ")";
+          })
+          .on('mouseover', barMouseOver)
+          .on('mouseout', barMouseOut)
+          .merge(bars)
+          .transition('barsAddBar')
+          .duration(750)
+          .attr('x', function (d, i) {
+              return x(i);
+          })
+          .attr("y", function (d) {
+              return h - y(d.value);
+          })
+          .attr("width", x.bandwidth())
+          .attr("height", function (d) {
+              return y(d.value);
+          })
+          .attr('fill', function (d) {
+              return "rgb(0,0, " + Math.floor(y(d.value)) + ")";
+          });
+
+      //TRANSITION LABELS
+
+      var text = svg.selectAll("text")
+          .data(barDataset);
+
+      text.enter()
+          .append("text")
+          .text(function (d) {
+              return d.value;
+          })
+          .attr("x", w + (x.bandwidth() / 2))
+          .attr("y", function (d) {
+              if (d.value >= 6) {
+                  return h - y(d.value) + 14;
+              } else {
+                  return h - y(d.value) - 4;
+              }
+          })
+          .attr("class", "barLabel")
+          .attr("fill", function (d) {
+              if (d.value >= 6) {
+                  return "white";
+              } else {
+                  return "black";
+              }
+          })
+          .merge(text)
+          .transition('textAddBar')
+          .duration(750)
+          .text(function (d) {
+              return d.value;
+          })
+          .attr("x", function (d, i) {
+              return x(i) + x.bandwidth() / 2;
+          })
+          .attr("y", function (d) {
+              if (d.value >= 6) {
+                  return h - y(d.value) + 14;
+              } else {
+                  return h - y(d.value) - 4;
+              }
+          });
+  }
+
+  function removeBar() {
+      barDataset.shift();
+
+      var bars = svg.selectAll("rect") //SELECT
+          .data(barDataset, key);
+
+      bars.exit() //EXIT
+          .transition('exitBars')
+          .duration(750)
+          .attr('x', -x.bandwidth()) //EXIT STAGE LEFT
+          .remove();
+
+      var text = svg.selectAll("text")
+          .data(barDataset, key);
+
+      text.exit() //EXIT
+          .transition('exitText')
+          .duration(750)
+          .attr('x', -x.bandwidth() / 2) //EXIT STAGE LEFT
+          .remove();
+
+      //UPDATE SCALES
+      x.domain(d3.range(barDataset.length));
+      y.domain([0, d3.max(barDataset, function (d) {
+          return d.value;
+      })]);
+
+      bars.transition('barsRemoveBar')
+          .duration(750)
+          .attr('x', function (d, i) {
+              return x(i);
+          })
+          .attr("y", function (d) {
+              return h - y(d.value);
+          })
+          .attr("width", x.bandwidth())
+          .attr("height", function (d) {
+              return y(d.value);
+          })
+          .attr('fill', function (d) {
+              return "rgb(0,0, " + Math.floor(y(d.value)) + ")";
+          });
+
+      text.transition('textRemoveBar')
+          .duration(750)
+          .text(function (d) {
+              return d.value;
+          })
+          .attr("x", function (d, i) {
+              return x(i) + x.bandwidth() / 2;
+          })
+          .attr("y", function (d) {
+              if (d.value >= 6) {
+                  return h - y(d.value) + 14;
+              } else {
+                  return h - y(d.value) - 4;
+              }
+          });
+  }
+
+  function sortBars() {
+      sorted = true;
 
       svg.selectAll("rect")
           .sort(function (a, b) {
-              if (sortOrder) {
-                  return d3.ascending(a.value, b.value);
-              } else {
-                  return d3.descending(a.value, b.value);
-              }
+              return d3.ascending(a.value, b.value);
           })
-          .transition()
-          .duration(1000)
+          .transition('sortBars')
+          .duration(750)
           .attr("x", function (d, i) {
               return x(i);
           });
 
       svg.selectAll("text")
           .sort(function (a, b) {
-              if (sortOrder) {
-                  return d3.ascending(a.value, b.value);
-              } else {
-                  return d3.descending(a.value, b.value);
-              }
+              return d3.ascending(a.value, b.value);
           })
-          .transition()
-          .duration(1000)
+          .transition('sortText')
+          .duration(750)
           .attr("x", function (d, i) {
               return x(i) + x.bandwidth() / 2;
           });
-  };
+      barDataset.sort(function (a, b) {
+          return a.value - b.value;
+      });
+  }
